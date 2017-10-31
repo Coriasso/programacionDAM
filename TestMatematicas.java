1ac742df7f7182a9bf3bc22a7b5f39db0584e5d4
Jorge Sanchez <coriasso@outlook.es>
2017-10-31 10:56:50 +0100

    Nuevo programa Matematicas con metodo esPar

---
+++ TestMatematicas.java
@@ -0,0 +1,8 @@
+public class TestMatematicas{
+	
+	public static void main(String[] args){
+		int numero = 17;
+		boolean resultado = Matematicas.esPar(numero);
+		System.out.println(resultado);	
+	};
+}
